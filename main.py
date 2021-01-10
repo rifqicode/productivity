72cec6b54641d249682269d21af4228cabbd4f53
Muhammad Rifqi <muhammadrifqi.tb@gmail.com>
2021-01-10 15:24:37 +0700

    add productivity

---
+++ main.py
@@ -0,0 +1,45 @@
+# author Muhammad Rifqi <muhammadrifqi.tb@gmail.com>
+
+import logging
+import sys
+import active_window
+import json
+
+
+def run():
+    window_list = {}
+
+    while 1:
+        current_window = active_window.get_active_window()
+        if current_window == '' or current_window == 'Task Switching':
+            continue
+            
+        window_name = active_window.get_window_name(current_window).strip()
+        if window_name in window_list:
+            window_list[window_name]['time'] += 1
+        else:
+            window_list[window_name] = {
+                'time' : 0
+            }
+        
+        print(window_list)
+
+def test():
+    dictionaries = {
+        'test' : {
+            'timer' : 100
+        },
+        'test2': {
+            'timer': 200
+        }
+    }
+
+    dictionaries['test3'] = {
+        'timer' : 100
+    }
+    dictionaries['test3'] = {
+        'timer' : 300
+    }
+    print(dictionaries)
+
+run()
